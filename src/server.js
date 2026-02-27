8a47b1d271b17cabdf50c6ecfa34898deae02b21
huudung-ngo <dung.ngohuu@gmail.com>
2026-02-27 09:43:16 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
+const app = require("./app");
 
+// Cloud Run sẽ inject PORT
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`🚀 OpenLab Platform running on port ${PORT}`);
+});
